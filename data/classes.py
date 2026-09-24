68de9989ddb099dc1e6e6592c71980fce7ab26d4
Oblachko90 <oblachkooo90@gmail.com>
2026-09-24 17:37:44 +0400

    Remove build artifacts, update classes/constants/main

--- data/classes.py
+++ data/classes.py
@@ -1,5 +1,5 @@
 # Picture Viewer
-# Copyright (C) 2026 oblachko
+# Copyright (C) 2026 Oblachko90
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
